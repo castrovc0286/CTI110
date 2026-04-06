b806af91e349e72d559f8851e188f2e273ddb0e3
castrovc0286 <castrovc0286@student.faytechcc.edu>
2026-04-05 21:55:19 -0400

    Add files via upload

---
+++ P1LAB1_CastroCristian.py
@@ -0,0 +1,8 @@
+# Cristian Castro
+# April 5,2026
+# P1LAB1_CastroCristian
+# Create Python program using Visual Studio Code
+
+name = input("Enter your first name: ")
+last_name= input("Enter your last name: ")
+print("Hello, " + name + " " + last_name +"!"+ " Welcome to CTI-110!")
